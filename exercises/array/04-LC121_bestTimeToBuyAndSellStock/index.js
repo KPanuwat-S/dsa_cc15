5825839139720a065a54e34978357a570cd352d6
KPanuwat-S <panuwatbood@gmail.com>
2023-11-11 22:51:20 +0700

    04 solution

--- exercises/array/04-LC121_bestTimeToBuyAndSellStock/index.js
+++ exercises/array/04-LC121_bestTimeToBuyAndSellStock/index.js
@@ -1,3 +1,27 @@
-function maxProfit(prices) {}
+function maxProfit(prices) {
+  let profit = 0;
+  let buy = prices[0];
+  let sell;
+  let currentProfit;
+
+  for (let i = 1; i < prices.length; i++) {
+    // move buy pointer --> also influence sell pointer
+    if (prices[i] < buy && i !== prices.length) {
+      buy = prices[i];
+      sell = prices[i + 1];
+      if (sell - buy > profit) {
+        profit = sell - buy;
+      }
+    }
+
+    // move sell pointer
+    if (prices[i] - buy > profit) {
+      profit = prices[i] - buy;
+      sell = prices[i];
+    }
+  }
+
+  return profit;
+}
 
 module.exports = maxProfit;
