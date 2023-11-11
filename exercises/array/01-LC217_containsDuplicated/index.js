759f83d1d8786c50ed60895931be792c46d48762
KPanuwat-S <panuwatbood@gmail.com>
2023-11-11 17:13:48 +0700

    01 solution

--- exercises/array/01-LC217_containsDuplicated/index.js
+++ exercises/array/01-LC217_containsDuplicated/index.js
@@ -1,3 +1,32 @@
-function containsDuplicate(arr) {}
+function containsDuplicate(arr) {
+  const obj = {};
+
+  // populate arr into obj
+  for (const num of arr) {
+    if (obj[num]) {
+      ++obj[num];
+    } else {
+      obj[num] = 1;
+    }
+  }
+
+  // now the obj will look like this
+  // {"1":1, "2":1} which the key is number and the value is the cout
+
+  // make use of obj key to access duplicated value
+
+  for (const key in obj) {
+    return obj[key] > 1;
+
+    // The code above is the same as below
+    // if (obj[key] > 1) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  }
+}
+
+containsDuplicate([1, 2, 3]);
 
 module.exports = containsDuplicate;
