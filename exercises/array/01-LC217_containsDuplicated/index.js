15ebd04dcb4cedf1259e9840fc5b2d4c6173df4e
KPanuwat-S <panuwatbood@gmail.com>
2023-11-11 20:19:52 +0700

    02 solution

--- exercises/array/01-LC217_containsDuplicated/index.js
+++ exercises/array/01-LC217_containsDuplicated/index.js
@@ -4,29 +4,13 @@ function containsDuplicate(arr) {
   // populate arr into obj
   for (const num of arr) {
     if (obj[num]) {
-      ++obj[num];
+      return true;
     } else {
       obj[num] = 1;
     }
   }
 
-  // now the obj will look like this
-  // {"1":1, "2":1} which the key is number and the value is the cout
-
-  // make use of obj key to access duplicated value
-
-  for (const key in obj) {
-    return obj[key] > 1;
-
-    // The code above is the same as below
-    // if (obj[key] > 1) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
-  }
+  return false;
 }
-
-containsDuplicate([1, 2, 3]);
 
 module.exports = containsDuplicate;
--- exercises/array/02-LC169_majorityElement/index.js
+++ exercises/array/02-LC169_majorityElement/index.js
@@ -1,3 +1,21 @@
-function majorityElement(arr) {}
+function majorityElement(arr) {
+  const obj = {};
+  let max = 0;
+
+  // populate obj
+  for (const num of arr) {
+    if (obj[num]) {
+      ++obj[num];
+
+      if (obj[num] > max) {
+        max = num;
+      }
+    } else {
+      obj[num] = 1;
+    }
+  }
+
+  return obj[max] > Math.trunc(arr.length / 2) ? max : "not found";
+}
 
 module.exports = majorityElement;
