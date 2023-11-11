4eac0138579d9e91f37f053ff3797ade4e78612d
KPanuwat-S <panuwatbood@gmail.com>
2023-11-11 22:44:04 +0700

    update 04 test case

--- exercises/array/04-LC121_bestTimeToBuyAndSellStock/test.js
+++ exercises/array/04-LC121_bestTimeToBuyAndSellStock/test.js
@@ -7,3 +7,7 @@ test("maxProfit([7, 1, 5, 3, 6, 4]) --> 5;", () => {
 test("expect(maxProfit([7, 6, 4, 3, 1])) --> 0;", () => {
   expect(maxProfit([7, 6, 4, 3, 1])).toEqual(0);
 });
+
+test("expect(maxProfit([7, 6, 4, 3, 1, 10])) --> 9;", () => {
+  expect(maxProfit([7, 6, 4, 3, 1, 10])).toEqual(9);
+});
