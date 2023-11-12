3f2067380a18d36bfda71b0faf81e74131a3b859
KPanuwat-S <panuwatbood@gmail.com>
2023-11-12 12:28:58 +0700

    5-7 solution

--- exercises/array/05-LC55_jumpGame/index.js
+++ exercises/array/05-LC55_jumpGame/index.js
@@ -1,3 +1,23 @@
-function canJump(nums) {}
+function canJump(nums) {
+  const n = nums.length;
+  let maxReach = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (i > maxReach) {
+      // If the current index cannot be reached, return false
+      return false;
+    }
+
+    // Update the maximum reachable index
+    maxReach = Math.max(maxReach, i + nums[i]);
+
+    // If the last index can be reached, return true
+    if (maxReach >= n - 1) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 module.exports = canJump;
