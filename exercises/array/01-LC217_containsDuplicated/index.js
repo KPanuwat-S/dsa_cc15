b423b91395f31907e1180e08b3fc6e11fbfa9698
Pavit Pimchanagul <pavitpim@gmail.com>
2023-11-14 18:27:20 +0700

    update:problem

--- exercises/array/01-LC217_containsDuplicated/index.js
+++ exercises/array/01-LC217_containsDuplicated/index.js
@@ -1,33 +1,3 @@
-// brute force
-// function containsDuplicate(arr) {
-//   // body
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = i + 1; j < arr.length; j++) {
-//       if (arr[i] === arr[j]) {
-//         return true;
-//       }
-//     }
-//     return false;
-//   }
-// }
-
-// Hash table
-
-function containsDuplicate(arr) {
-  const obj = {};
-
-  // O(N)
-  for (const element of arr) {
-    if (obj[element]) {
-      // O(1)
-      return true;
-    } else {
-      // O(1)
-      obj[element] = 1;
-    }
-  }
-
-  return false;
-}
+function containsDuplicate(arr) {}
 
 module.exports = containsDuplicate;
