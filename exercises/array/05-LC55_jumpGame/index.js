ef18cb5f37f8aed81670c2d4ff69e7defa961e39
KPanuwat-S <panuwatbood@gmail.com>
2023-11-14 17:28:09 +0700

    update dynamic programming solution

--- exercises/array/05-LC55_jumpGame/index.js
+++ exercises/array/05-LC55_jumpGame/index.js
@@ -1,3 +1,26 @@
-function canJump(nums) {}
+function canJump(nums) {
+  // body
+  let maxReach = 0;
+  let n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    // guard clause
+    if (nums[i] + i >= n - 1) {
+      return true;
+    }
+
+    // compare max reach
+    if (nums[i] + i > maxReach) {
+      maxReach = nums[i] + i;
+    }
+    // the above code is the as below 
+    // maxReach = Math.max(maxReach, i + nums[i]);
+
+    // Check before moving
+    if (maxReach < i + 1) {
+      return false;
+    }
+  }
+}
 
 module.exports = canJump;
